358fc68686c25e0a7938bac4d708ca3e9df154c7
Tru7h <Tru7h@users.noreply.github.com>
2017-09-17 00:39:32 -0500

    ping pong bot is named Madelyn with client shutdown

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
  "use strict";
 
+ const Discord = require("discord.js");
+ const parser = require("ini-parser");
+ parser.parse(String)
+ const config = parser.parseFileSync('config.ini')
+ const client = new Discord.Client();
+ 
+ client.on("ready", () => {
+   console.log("I am ready!");
+ });
+ 
+ client.on("message", (message) => {
+   if (message.content.startsWith("ping")) {
+     message.channel.send("pong!");
+   }
+ });
+ 
+ client.on("message", (message) => {
+  if (message.content.startsWith("!username")) {
+    client.user.setUsername("Madelyn")
+    message.channel.send('my name is Maddie Addie!')
+  }
+ });
+
+ client.on("message", (message) => {
+   if (message.content.startsWith("!shutdown")) {
+    message.channel.send("see ya!")
+    client.destroy()
+   }
+ })
+
+ console.log("attempting token " + config.client.token)
+ client.login(config.client.token);
